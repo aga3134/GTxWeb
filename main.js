fcef23eb3644470c6330c34a4d83838e605d215f
aga3134 <aga3134@gmail.com>
2018-12-07 12:47:33 +0800

    add btraj graph interaction

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ var g_APP = new Vue({
     selectImage: "image/no-image.png",
     btrajData: [],
     btrajObs: [],
+    btrajMaxValue: 0,
     btrajImages: [],
     ftrajImages: [],
     stationArr: [],
@@ -62,6 +63,7 @@ var g_APP = new Vue({
             d.company_name="其他工廠源";
             break;
         }
+        this.$set(d, "active", true);
       }
       this.companyArr = this.companyArr.filter(function(d){
         return d.color != null;
@@ -69,12 +71,12 @@ var g_APP = new Vue({
       this.companyArr.sort(function(a, b) {
         return a.serial - b.serial;
       });
-      console.log(this.companyArr);
-    }.bind(this));
 
-    google.maps.event.addDomListener(window, 'load', this.InitMap);
-    this.UpdateImage();
-    this.UpdateBtrajGraph();
+      google.maps.event.addDomListener(window, 'load', this.InitMap);
+      this.UpdateImage();
+      this.UpdateBtrajGraph();
+    }.bind(this));
+    
   },
   methods: {
     InitMap: function(){
@@ -180,11 +182,25 @@ var g_APP = new Vue({
       this.selectImage = url;
       this.showImageBox = true;
     },
+    SwitchCompany: function(i){
+      this.companyArr[i].active = !this.companyArr[i].active;
+      this.DrawBtrajGraph();
+    },
+    SwitchCompanyAll: function(){
+      for(var i=0;i<this.companyArr.length;i++){
+        this.companyArr[i].active = !this.companyArr[i].active;
+      }
+      this.DrawBtrajGraph();
+    },
     UpdateBtrajGraph: function(){
       var selectDate = moment(this.currentDate,"YYYY-MM-DD");
       var minDate = selectDate.clone().add(-6, 'days').format("YYYYMMDD");
       var curDate = selectDate.format("YYYYMMDD");
       var maxDate = selectDate.clone().add(3, 'days').format("YYYYMMDD");
+      this.btrajMaxValue = 0;
+      var companyHash = d3.nest()
+          .key(function(d){return d.company_id;})
+          .map(this.companyArr);
 
       var url = "/data/btraj.php?expMin="+minDate;
       url += "&expMax="+curDate;
@@ -201,15 +217,18 @@ var g_APP = new Vue({
           var expDate = this.btrajData[expKey];
           for(var dateKey in expDate){
             var date = expDate[dateKey];
+            var pmf = parseFloat(date["PMfstr"][0].PMf);
+            if(pmf > this.btrajMaxValue) this.btrajMaxValue = pmf;
             if(!("PMfstrS" in date)) continue;
             for(var companyKey in date){
-              if(date[companyKey].serial < 6) continue;
-              date["PMfstrS"].PMf -= date[companyKey].PMf;
+              if(!(companyKey in companyHash)) continue;
+              date[companyKey][0].date = dateKey;
+              date[companyKey][0].company_name = companyHash[companyKey][0].company_name;
+              if(companyHash[companyKey][0].serial < 6) continue;
+              date["PMfstrS"][0].PMf -= date[companyKey][0].PMf;
             }
           }
         }
-
-        console.log(this.btrajData);
 
         url = "/data/obs.php?minDate="+minDate;
         url += "&maxDate="+maxDate;
@@ -219,7 +238,6 @@ var g_APP = new Vue({
           this.btrajObs = d3.nest()
             .key(function(d){return d.d;})
             .map(this.btrajObs);
-          console.log(this.btrajObs);
 
           this.DrawBtrajGraph();
         }.bind(this));
@@ -231,12 +249,13 @@ var g_APP = new Vue({
       var curDate = selectDate.format("YYYYMMDD");
       var maxDate = selectDate.clone().add(3, 'days').format("YYYYMMDD");
 
+      var upperBound = parseInt((this.btrajMaxValue+10)*0.1)*10;
       var graph = $("#btrajGraph");
       var w = graph.width(), h = graph.height();
       var svg = d3.select("#btrajGraph");
       svg.selectAll("*").remove();
-      var padL = 140, padR = 10, padT = 10, padB = 15;
-      var scaleH = d3.scale.linear().domain([0,100]).range([h-padT-padB,0]);
+      var padL = w>500?140:25, padR = 10, padT = 10, padB = 15;
+      var scaleH = d3.scale.linear().domain([0,upperBound]).range([h-padT-padB,0]);
       var wStep = (w-padL-padR)*0.1;
 
       //draw axis
@@ -285,17 +304,42 @@ var g_APP = new Vue({
         for(var j=1;j<company.length;j++){
           var c = company[j].company_id;
           if(! (c in data)) continue;
+          if(!company[j].active) continue;
           var dataH = h-padT-padB-scaleH(data[c][0].PMf);
+          var x = offsetX*wStep+padL+5;
+          var y = h-offsetY-dataH-padB;
+
           rectGroup.append('rect').attr({
             "width": wStep-5, 
             "height": dataH,
-            "x": offsetX*wStep+padL+5, 
-            "y": h-offsetY-dataH-padB
+            "x": x, 
+            "y": y,
+            "data-color": company[j].color,
+            "data-pmf": data[c][0].PMf,
+            "data-company": company[j].company_name,
+            "data-date": data[c][0].date,
           }).style({
             "fill": company[j].color,
             "stroke-width": 0,
             "opacity": 0.8
+          }).on("mouseover", function(){
+            var color = d3.select(this).attr("data-color");
+            var pmf = parseFloat(d3.select(this).attr("data-pmf"));
+            var company = d3.select(this).attr("data-company");
+            var date = d3.select(this).attr("data-date");
+            var x = d3.select(this).attr("x");
+            var y = d3.select(this).attr("y");
+            $("#hoverDialog").css({
+              "border-color":color,
+              "opacity": 1
+            }).html(company+"<br>"+date+"<br>"+pmf.toFixed(2));
+          })
+          .on("mouseout", function(){
+            $("#hoverDialog").css({
+              "opacity": 0
+            });
           });
+          
           offsetY += dataH;
         }
       }
@@ -321,6 +365,9 @@ var g_APP = new Vue({
 
       var obsGroup = svg.append("g");
       var lineData = [];
+      var cutIndex = 0;
+      if(w < 400) cutIndex = 8;
+      else if(w < 800) cutIndex = 5;
       for(var i=-6;i<=3;i++){
         var date = selectDate.clone().add(i, 'days').format("YYYY-MM-DD");
         var data = this.btrajObs[date];
@@ -328,33 +375,37 @@ var g_APP = new Vue({
           x:(i+6.5)*wStep+padL,
           y:scaleH(parseFloat(data[0].obs))+padT,
           value: parseInt(data[0].obs),
-          date: date
+          date: date.substring(cutIndex)
         });
       }
-      obsGroup.append("path")
-        .attr("fill", "none")
-        .attr("stroke", "#ff0000")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 2)
-        .attr("d", line(lineData));
+      if(this.companyArr[0].active){
+        obsGroup.append("path")
+          .attr("fill", "none")
+          .attr("stroke", "#ff0000")
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-linecap", "round")
+          .attr("stroke-width", 2)
+          .attr("d", line(lineData));
+      }
 
       for(var i=0;i<lineData.length;i++){
-        obsGroup.append("circle").attr({
-          "cx": lineData[i].x,
-          "cy": lineData[i].y,
-          "r": 5,
-          "fill": "#ff0000",
-          "stroke": "none"
-        });
-        obsGroup.append("text").attr({
-          "x": parseInt(lineData[i].x),
-          "y": parseInt(lineData[i].y-10),
-          "text-anchor":"middle",
-        }).style({
-          "fill": "#ff0000",
-          "font-size": " 12px"
-        }).text(lineData[i].value);
+        if(this.companyArr[0].active){
+          obsGroup.append("circle").attr({
+            "cx": lineData[i].x,
+            "cy": lineData[i].y,
+            "r": 5,
+            "fill": "#ff0000",
+            "stroke": "none"
+          });
+          obsGroup.append("text").attr({
+            "x": parseInt(lineData[i].x),
+            "y": parseInt(lineData[i].y-10),
+            "text-anchor":"middle",
+          }).style({
+            "fill": "#ff0000",
+            "font-size": " 12px"
+          }).text(lineData[i].value);
+        }
 
         obsGroup.append("text").attr({
           "x": parseInt(lineData[i].x),
@@ -366,6 +417,7 @@ var g_APP = new Vue({
           "font-size": " 12px"
         }).text(lineData[i].date);
       }
+
     }
   }
 });
@@ -378,4 +430,5 @@ window.addEventListener('resize', function(e) {
   g_APP.openSidePanel = false;
   var mainContent = $(".main-content");
   mainContent.css("width","100%");
+  g_APP.DrawBtrajGraph();
 });
